9cbc3eee65c27870b64351e54ad011a0b0a37cbb
Valyon1886 <64031088+Valyon1886@users.noreply.github.com>
2021-10-12 09:49:35 +0300

    Adding func to play new file + some changing 'Politeness'

--- Song.py
+++ Song.py
@@ -1,9 +1,8 @@
-import pydub
 from pydub import AudioSegment
+
 
 class Sound(object):
     """Class - wave-file init"""
-
     def __init__(self, path, begin_time, end_time):
         """Constructor"""
         self.path = path
@@ -15,6 +14,6 @@ class Sound(object):
         """a+b=simple"""
         sound1 = AudioSegment.from_wav(self.path)
         sound2 = AudioSegment.from_wav(other.path)
-        sound1 = sound1[self.begin_time*1000:self.end_time*1000]
-        sound2 = sound2[other.begin_time*1000:other.end_time*1000]
+        sound1 = sound1[int(self.begin_time)*1000:int(self.end_time)*1000]
+        sound2 = sound2[int(other.begin_time)*1000:int(other.end_time)*1000]
         return  sound1 + sound2
--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-
 from Song import Sound
+import warnings
+from pydub.playback import play
+from playsound import playsound
+warnings.filterwarnings("ignore")
+
 
 def initiate_of_shift():
     print(f'The program converts two audio files (of the wav type into a single file)\n')
-    '''   
+    '''
+
+    #For tests
+    type = 'wav'
+    name1 = "C:\\Users\\Valentin\\Music\\1"
+    name2 = 'C:\\Users\\Valentin\\Music\\2'
+    end_name = 'C:\\Users\\Valentin\\Music\\prop'
+    start_time1 = 0
+    start_time2 = 0
+    end_time1=3
+    end_time2=3
+    '''
+
     print(f'Enter the path (without type) to the first audio file:')
     name1 = input()
     print(f'Enter the start time of the first file:')
@@ -20,15 +36,8 @@ def initiate_of_shift():
 
     print(f'Enter the name(endpath/name) (without type) of the finished file:')
     end_name = input()
-    '''
+
     type = 'wav'
-    name1 = r"C:\Users\Valentin\Music\1"
-    name2 = r'\Users\Valentin\Music\2'
-    end_name = r'\Users\Valentin\Music\3'
-    start_time1 = 1
-    start_time2 = 2
-    end_time1=4
-    end_time2=5
 
     file1 = Sound(name1+'.'+type, start_time1, end_time1)
     file2 = Sound(name2+'.'+type, start_time2, end_time2)
@@ -37,6 +46,15 @@ def initiate_of_shift():
     end_file.export(end_name+".wav", format='wav')
     print(f'The final file is located at: ' + end_name+".wav")
 
+    print('Do you wanna play it? (Yes/No)')
+    a = input()
+    if(a=="Yes"):
+        print("Enter your file path: ")
+        b = input()
+        print(" ♫•*¨*•.¸¸♪ (^_^♪) ")
+        playsound(b+".wav")
+    if(a == "No"):
+        print("  ((╬◣﹏◢╬))  ")
 if __name__ == '__main__':
     initiate_of_shift()
 
