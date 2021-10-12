464e95e98940cb9a8e56d43705d9ad5a3b979aab
Valyon1886 <64031088+Valyon1886@users.noreply.github.com>
2021-10-12 08:41:39 +0300

    End of debugging + some changing 'Politeness'

--- main.py
+++ main.py
@@ -2,19 +2,16 @@
 from Song import Sound
 
 def initiate_of_shift():
-    print(f'The program converts two audio files (of the same type into a single file)\n')
-    '''
-    print(f'Enter the file type:')
-    type = input()
-    '''
-    print(f'Enter the path to the first audio file:')
+    print(f'The program converts two audio files (of the wav type into a single file)\n')
+    '''   
+    print(f'Enter the path (without type) to the first audio file:')
     name1 = input()
     print(f'Enter the start time of the first file:')
     start_time1 = input()
     print(f'Enter the end time of the first file:')
     end_time1 = input()
 
-    print(f'Enter the path to the second audio file:')
+    print(f'Enter the path (without type) to the second audio file:')
     name2 = input()
     print(f'Enter the start time of the second file:')
     start_time2 = input()
@@ -23,13 +20,22 @@ def initiate_of_shift():
 
     print(f'Enter the name(endpath/name) (without type) of the finished file:')
     end_name = input()
+    '''
+    type = 'wav'
+    name1 = r"C:\Users\Valentin\Music\1"
+    name2 = r'\Users\Valentin\Music\2'
+    end_name = r'\Users\Valentin\Music\3'
+    start_time1 = 1
+    start_time2 = 2
+    end_time1=4
+    end_time2=5
 
     file1 = Sound(name1+'.'+type, start_time1, end_time1)
     file2 = Sound(name2+'.'+type, start_time2, end_time2)
 
     end_file = file1 + file2
-    end_file.export(end_name+".wav" '''+type''', format='wav')
-    print(f'The final file is located at: ' + end_name+".wav")#+type)
+    end_file.export(end_name+".wav", format='wav')
+    print(f'The final file is located at: ' + end_name+".wav")
 
 if __name__ == '__main__':
     initiate_of_shift()
